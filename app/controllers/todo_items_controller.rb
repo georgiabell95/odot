5c0cfae23ae90013b412cf16faf823e9e0659dd6
Georgia Bell <georgia@digitalreflow.co.uk>
2016-06-21 15:52:02 +0100

    Mark todo item as completed

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -46,7 +46,7 @@ def update
 
   def complete
     @todo_item = @todo_list.todo_items.find(params[:id])
-    @todo_item.update_attribute(completed_at. Time.now)
+    @todo_item.update_attribute(:completed_at, Time.now)
     redirect_to todo_list_todo_items_path, notice: "Todo item marked as complete"
   end
 
