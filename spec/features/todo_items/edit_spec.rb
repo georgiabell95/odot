fad0289df60ced11d6d7f816eddf430d755c3c1c
Georgia Bell <georgia@digitalreflow.co.uk>
2016-06-21 12:22:59 +0100

    add specs and cide for editing to do items

--- spec/features/todo_items/edit_spec.rb
+++ spec/features/todo_items/edit_spec.rb
@@ -16,12 +16,24 @@ describe "Editing todo items" do
 		within("#todo_item_#{todo_item.id}") do
 			click_link "Edit"
 		end
+		fill_in "Content", with: "Lots of Milk"
+		click_button "Save"
+		expect(page).to_ have_content("Saved todo list item")
+		todo_item.reload
+		expect(todo_item.content).to eq("Lots of Milk")
+	end
+
+	it "is unsuccessful with no content" do
+		visit_todo_list(todo_list)
+		within("#todo_item_#{todo_item.id}") do
+			click_link "Edit"
+		end
 		fill_in "Content", with: ""
 		click_button "Save"
 		expect(page).to_not have_content("Saved todo list item")
 		expect(page).to have_content("Content can't be blank")
 		todo_item.reload
-		expect(todo_item.content).to eq("Lots of Milk")
+		expect(todo_item.content).to eq("Milk")
 	end
 
 end
